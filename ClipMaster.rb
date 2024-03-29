1263020c2622b6053fbfc13544dc2c6e9e419a5a
Mussabaheen Malik <30601645+Mussabaheen@users.noreply.github.com>
2024-03-29 15:09:19 +0500

    Update ClipMaster.rb

--- ClipMaster.rb
+++ ClipMaster.rb
@@ -1,8 +1,8 @@
 class Clipmaster < Formula
   desc "App to maintain the clipboard history for macOS"
   homepage "https://github.com/Mussabaheen/clipboard"
-  url "https://github.com/Mussabaheen/ClipMaster/archive/refs/tags/0.0.5.tar.gz"
-  sha256 "41fdc7d1a19b4a8bcb848169c1577dc692e7cb5af88ddd7da55872982592d4bb"
+  url "https://github.com/Mussabaheen/ClipMaster/archive/refs/tags/0.0.6.tar.gz"
+  sha256 "840a8ef5e4665c7e569269360f267bc3783a8a0a437d533ea7ee1be13d56ff59"
   license "MIT"
 
   depends_on "go" => :build
